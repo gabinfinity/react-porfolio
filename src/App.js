9fb5f2ea617138c59ab3d4fa223300f2f60fab71
gabinfinity <59806981+gabinfinity@users.noreply.github.com>
2023-06-19 06:19:16 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import BotaoCv from './componentes/botao-cv';
+import BotaoGithub from './componentes/botao-github';
+import BotaoLinkedin from './componentes/botao-linkedin';
+import BotaoMail from './componentes/botao-mail';
+
+function App() {
+  return (
+    <div className="app">
+      <section className="profile-container">
+        <div className="profile-icons">
+          <a href="https://www.linkedin.com/" target='blank'>
+            <BotaoLinkedin />
+          </a>
+          <a href="https://github.com/gabinfinity" target='blank'>
+            <BotaoGithub />
+          </a>
+          <a href="mailto:gabmzni@gmail.com" target='blank'>
+            <BotaoMail />
+          </a>
+          <a href="Curriculo.pdf" target='blank'>
+            <BotaoCv />
+          </a> 
+        </div>
+        <div className="profile-infos" >
+
+          <h1>OLÁ, MEU NOME É GABRIEL</h1>
+          <h2>FULLSTACK DEVELOPER</h2>
+          <h3>TENHO 23 ANOS E JÁ SOU DESENVOLVEDOR BACKEND FREELANCER HÁ ALGUNS ANOS. ESTOU INICIANDO AGORA A MINHA JORNADA COM FRONT-END.</h3>
+
+        </div>
+        <img className="profile-image" src="https://github.com/gabinfinity.png" alt="Foto de Gabriel" />
+      </section>    
+    </div>
+  );
+}
+
+export default App;
